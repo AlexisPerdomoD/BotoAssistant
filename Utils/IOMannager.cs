e4870359118e33275a145f83ab8a4ef7c493a271
alexisperdomod <dropdibemol@gmail.com>
2024-11-24 10:27:06 -0500

    updates utils clases

--- Utils/IOMannager.cs
+++ Utils/IOMannager.cs
@@ -1,30 +1,44 @@
 using Microsoft.Extensions.Logging;
+using Boto.Models;
+
 namespace Boto.Utils;
-
-
-
-public enum LogCategory
-{
-    ExternalError,
-    InternalError,
-    Information,
-    Warning,
-    Error,
-    Critical,
-    Debug,
-}
-
-public interface IBotoLogger : ILogger
-{
-    public void LogDebug(string message);
-    public void LogInformation(string message, bool? time);
-    public void LogWarning(string message, bool? time);
-    public void LogError(string message, Exception e);
-    public void LogCritical(string message, Exception e);
-}
-public class BotoMainLogger(LogLevel logLevel) : IBotoLogger
+public class MainIOMannager(LogLevel logLevel) : IBotoLogger, IInputOutputMannager
 {
     private readonly LogLevel _logLevel = logLevel;
+    public string? LastInput { get; private set; }
+    public List<string> History { get; private set; } = [];
+
+    public string? GetInput(string prompt, Func<string?, bool>? validator = null, string? customTryAgainMessage = null)
+    {
+        validator ??= static i => !string.IsNullOrWhiteSpace(i);
+        this.LogInformation(prompt, false);
+        string? input = Console.ReadLine();
+        int tries = 0;
+        while (!validator(input))
+        {
+            this.LogInformation(customTryAgainMessage ?? "Invalid input. Please try again.", false);
+            input = Console.ReadLine();
+            tries++;
+            if (tries > 3)
+            {
+                this.LogInformation("Too many tries. Ending selection.", true);
+                input = null;
+                break;
+            }
+        }
+        if (input is not null)
+        {
+            this.LastInput = input;
+            this.History.Add(input);
+        }
+        return input;
+    }
+    public void ClearHistory()
+    {
+        this.History.Clear();
+        this.LastInput = null;
+    }
+
     private string _defaultFormater(string message, Exception? exception)
     {
         if (exception != null)
@@ -36,9 +50,7 @@ public class BotoMainLogger(LogLevel logLevel) : IBotoLogger
         }
         return $"{message}";
     }
-
-
-    public bool IsEnabled(LogLevel logLevel) => logLevel >= LogLevel.Information;
+    public bool IsEnabled(LogLevel logLevel) => logLevel >= this._logLevel;
     // Para simplificar, no implementamos un scope. Si lo necesitas, puedes devolver un objeto IDisposable.
     public IDisposable BeginScope<TState>(TState state) where TState : notnull => null!;
 
@@ -51,13 +63,13 @@ public class BotoMainLogger(LogLevel logLevel) : IBotoLogger
         Console.WriteLine(logMessage);
     }
 
-    public void LogInformation(string message, bool? time)
+    public void LogInformation(string message, bool? time = false)
     {
         string logMessage = time == true ? $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {message}" : message;
         this.Log(LogLevel.Information, default, logMessage, null, this._defaultFormater);
     }
 
-    public void LogWarning(string message, bool? time)
+    public void LogWarning(string message, bool? time = false)
     {
         string logMessage = time == true ? $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {message}" : message;
         this.Log(LogLevel.Warning, default, logMessage, null, this._defaultFormater);
@@ -68,6 +80,4 @@ public class BotoMainLogger(LogLevel logLevel) : IBotoLogger
         => this.Log(LogLevel.Critical, default, message, e, this._defaultFormater);
     public void LogDebug(string message)
         => this.Log(LogLevel.Debug, default, message, null, this._defaultFormater);
-
-
 }
