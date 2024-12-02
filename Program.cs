24133e9688d5feb4c01482177690d7ca12a508ee
alexisperdomod <dropdibemol@gmail.com>
2024-12-01 22:11:58 -0500

    feat: runnning program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Immutable;
+
+using Boto.Utils;
+using Boto.Services;
+using Boto.Models;
+
+MainIOMannager iom = new(LogLevel.Information);
+UsrMngr usrMngr = new(iom);
+MainService mainService = new(iom, "Boto", "Main Service", ImmutableDictionary<string, IServiceOption>.Empty, usrMngr);
+await mainService.Run();
+mainService.GoodBye();
+
