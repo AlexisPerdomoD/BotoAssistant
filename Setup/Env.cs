b81fec4ea76e65a1ce2aac46c7862936e99498df
alexisperdomod <dropdibemol@gmail.com>
2024-12-28 22:50:50 -0500

    feat: Env variables are storage on a record class type

--- Setup/Env.cs
+++ Setup/Env.cs
@@ -1,9 +1,15 @@
 namespace Boto.Setup;
 
-public class Env
+public record Env
 {
-    public static string WorkingDirectory =>
+    public static readonly string AppMode =
+        Environment.GetEnvironmentVariable("BOTO_APP_MODE") ?? "DEV";
+
+    public static readonly string WorkingDirectory =
         Environment.GetEnvironmentVariable("BOTO_WORKING_DIRECTORY")
         ?? throw new InvalidOperationException("BOTO_WORKING_DIRECTORY is not set");
-    public static string AppMode => Environment.GetEnvironmentVariable("BOTO_APP_MODE") ?? "DEV";
+
+    public static readonly string? GeminiApiKey = Environment.GetEnvironmentVariable(
+        "BOTO_GEMINI_API_KEY"
+    );
 }
