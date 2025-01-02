67856164b9585a169102ebc4d55fbbdbcc32a761
alexisperdomod <dropdibemol@gmail.com>
2025-01-01 19:42:09 -0500

    feat: impplement properly InmutableDictionary for Main program options

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections.Immutable;
 using Boto.Models;
 using Boto.Services;
+using Boto.Services.Gemini;
+using Boto.Services.ServiceOption;
 using Boto.Utils;
 using Microsoft.Extensions.Logging;
 
-MainIOMannager iom = new(LogLevel.Information);
-UsrMngr usrMngr = new(iom);
-MainService mainService =
-    new(iom, "Boto", "Main Service", ImmutableDictionary<string, IServiceOption>.Empty, usrMngr);
-await mainService.Run();
+var iom = new MainIOMannager(LogLevel.Information);
+var usrMngr = new UsrMannager(iom);
+var gemini = new GeminiService(iom, usrMngr);
+var MainOptions = new Dictionary<string, IServiceOption>
+{
+    {
+        "gemini",
+        new ServiceOpt(
+            name: "gemini",
+            description: "Gemini IA assistant Service, this module includes calling the API, list old conversations, etc",
+            cleanConsoleRequired: true,
+            exec: async _ => await gemini.Run()
+        )
+    }
+}.ToImmutableDictionary();
 
+MainService mainService = new(iom, "Boto", "Main Service", MainOptions, usrMngr);
+await mainService.Run();
 mainService.GoodBye();
